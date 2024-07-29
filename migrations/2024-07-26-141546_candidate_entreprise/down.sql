228cfd24f994a29c8bf746bf66e49e961553eb8a
LaMaxMenace <maxence.vast@free.fr>
2024-07-29 18:46:07 +0200

    fix/remove sql migration

--- migrations/2024-07-26-141546_candidate_entreprise/down.sql
+++ migrations/2024-07-26-141546_candidate_entreprise/down.sql
@@ -1,9 +1,3 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE entreprise;
 DROP TABLE candidate;
-
-ALTER TABLE users
-DROP CONSTRAINT IF EXISTS fk_entreprise;
-
-ALTER TABLE users
-DROP COLUMN IF EXISTS entreprise_id;
